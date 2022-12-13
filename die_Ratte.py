39b70d5f2b6258d37ad32c5aca1aea4cf1b222b9
Sergei Shcherbak <sshch1987@gmail.com>
2022-12-13 18:48:16 +0200

    Update die_Ratte.py

--- die_Ratte.py
+++ die_Ratte.py
@@ -1,38 +1,42 @@
 import pyautogui
+# import gettext
 
 # Temporary debug commands:
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
-# Then keep coding:
-# print(pyautogui.size())
 
-# let's first keep the screen size automatic:
-width, height = pyautogui.size()
-# moving mouse:
-for i in range(10):  # range показывает количество циклов мыши
-    pyautogui.moveTo(100, 100, duration=0.25)  # перемещаем мышь в заданную позиицю на экране
-    # duration - скорость перемещения. При '0' - перемещается мгновенно
-    print(pyautogui.position())  # temporary checking command
-    pyautogui.moveTo(200, 100, duration=0.25)
-    print(pyautogui.position())
-    pyautogui.moveTo(200, 200, duration=0.25)
-    print(pyautogui.position())
-    pyautogui.moveTo(100, 200, duration=0.25)
-    print(pyautogui.position())
-
-for i in range(10):
-    # то же самое, но курсор перемещается относительно текущей позиции
-    # начинает квадрат с того места, где мышь оказывается на экране, когда запускается код
-    pyautogui.moveRel(100, 0, duration=0.25)
-    print(pyautogui.position())
-    pyautogui.moveRel(0, 100, duration=0.25)
-    print(pyautogui.position())
-    pyautogui.moveRel(-100, 0, duration=0.25)
-    print(pyautogui.position())
-    pyautogui.moveRel(0, -100, duration=0.25)
-    print(pyautogui.position())
+def screen_size():
+    print(pyautogui.size())
 
 
-# if __name__ == '__main__':
-#     pass
+# def set_screen_size():
+# # let's first keep the screen size automatic:
+#     width, height = pyautogui.size()
+# # moving mouse:
+# for i in range(10):  # range показывает количество циклов мыши
+#     pyautogui.moveTo(100, 100, duration=0.25)  # перемещаем мышь в заданную позиицю на экране
+#     # duration - скорость перемещения. При '0' - перемещается мгновенно
+#     print(pyautogui.position())  # temporary checking command
+#     pyautogui.moveTo(200, 100, duration=0.25)
+#     print(pyautogui.position())
+#     pyautogui.moveTo(200, 200, duration=0.25)
+#     print(pyautogui.position())
+#     pyautogui.moveTo(100, 200, duration=0.25)
+#     print(pyautogui.position())
+#
+# for i in range(10):
+#     # то же самое, но курсор перемещается относительно текущей позиции
+#     # начинает квадрат с того места, где мышь оказывается на экране, когда запускается код
+#     pyautogui.moveRel(100, 0, duration=0.25)
+#     print(pyautogui.position())
+#     pyautogui.moveRel(0, 100, duration=0.25)
+#     print(pyautogui.position())
+#     pyautogui.moveRel(-100, 0, duration=0.25)
+#     print(pyautogui.position())
+#     pyautogui.moveRel(0, -100, duration=0.25)
+#     print(pyautogui.position())
+
+
+if __name__ == '__main__':
+    screen_size()
